de51749521e0468366d28d3a8c43d8fac8eb8e75
Gustavo Caldas Silveira Borges <80328652+distnrb@users.noreply.github.com>
2021-06-15 14:23:49 -0300

    Add files via upload
    6º Lista de Algoritmos e Programação

---
+++ Lista 6/Q04.cpp
@@ -0,0 +1,19 @@
+/*
+4. Foi feita uma pesquisa para determinar o índice de mortalidade infantil em um certo período. Fazer um programa que:
+-leia o número de crianças nascidas no período;
+-leia um número indeterminado de linhas com as informações das crianças que morreram, contendo, cada uma, o sexo da criança 
+(m -masc., f- fem.) e o número de meses de vida da criança. A última linha, que não entrará nos cálculos, contém no lugar do 
+sexo o caractere v (vazio). Determine e escreva:
+
+a) porcentagem de crianças mortas no período;
+b) porcentagem de crianças masculinas mortas no período;
+c) porcentagem de crianças que viveram 24 meses ou menos no período
+*/
+#include<stdio.h>
+#include<locale.h>
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    
+    return 0;
+    }
+
