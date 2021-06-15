323cc4811619d54c4583dc45f0db998f8c25f14b
Gustavo Caldas Silveira Borges <80328652+distnrb@users.noreply.github.com>
2021-06-15 11:31:31 -0300

    Add files via upload
    Lista 4 de algoritmos e programação.

---
+++ Lista 4/Q10.cpp
@@ -0,0 +1,12 @@
+/*10 – Construa um algoritmo que permita fazer um levantamento do estoque de vinhos de uma adega,
+tendo como dados de entrada tipos de vinho, sendo: ‘T’ para tinto, ‘B’ para branco e ‘R’ para rosê.
+Especifique a porcentagem de cada tipo sobre o total geral de vinhos; a quantidade de vinhos é desconhecida,
+utilize como finalizador ‘F’ de fim.*/
+#include<stdio.h>
+#include<locale.h>
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    int total, tinto, branco, rose;
+    return 0;
+    }
+
